6d7b9bec91c4ad2a4680f5e8c678439b4278fe25
baimo <1966758748@qq.com>
2016-06-06 13:37:12 +0800

    models

--- my_blog/article/admin.py
+++ my_blog/article/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from article.models import Category, Tag
+from article.models import Blog, Comment
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('tag_name', 'create_time')
+
+
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'publish_time', 'update_time',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('blog', 'name', 'email', 'created',) 
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+
--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -4,15 +4,15 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
 
-@python_2_unicode_compatible
-class Author(models.Model):
-    """docstring for Author"""
-    name = models.CharField(max_length=30)
-    email = models.EmailField(blank=True)
-    website = models.URLField(blank=True)
+# @python_2_unicode_compatible
+# class Author(models.Model):
+#     """docstring for Author"""
+#     name = models.CharField(max_length=30)
+#     email = models.EmailField(blank=True)
+#     website = models.URLField(blank=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
 @python_2_unicode_compatible
@@ -35,7 +35,7 @@ class Tag(models.Model):
 @python_2_unicode_compatible
 class Blog(models.Model):
     title = models.CharField('标题', max_length=100)
-    author = models.ForeignKey(Author)
+    # author = models.ForeignKey(Author)
     content = models.TextField('正文', blank=True, null=True)
     category = models.ForeignKey(Category, verbose_name='分类')
     tags = models.ManyToManyField(Tag, verbose_name='标签')
@@ -52,5 +52,6 @@ class Blog(models.Model):
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, verbose_name='博客')
     name = models.CharField('称呼', max_length=16)
+    email = models.EmailField(blank=True)
     content = models.CharField('内容', max_length=140)
     created = models.DateTimeField('发布时间', auto_now_add=True)
